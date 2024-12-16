d5523e09a7fb9864b3a30f5d1718604ba7628a91
Achrafness <achraf.nessighaoui13@gmail.com>
2024-12-16 21:57:12 +0100

    Initial project setup

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
-
+const connectDB = require("./db/connect");
 
 
 // Middleware uses
@@ -34,7 +34,7 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
-    
+    await connectDB(process.env.MONGO_URL);
     app.listen(port, () => {
       console.log(`Server is listening on port ${port}...`);
       console.log(`Environment: ${process.env.NODE_ENV || 'development'}`);

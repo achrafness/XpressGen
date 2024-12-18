c4d8fb7c76e4eb6b609eca6350a71c719516cdb5
achrafness <achraf.nessighaoui13@gmail.com>
2024-12-18 11:18:56 +0100

    add detect os for run cmd

--- src/utils/command_runner.py
+++ src/utils/command_runner.py
@@ -1,16 +1,32 @@
 import subprocess
 import logging
 from typing import List
+import os
+import platform
 
 class CommandRunner:
     def __init__(self, logger: logging.Logger):
         self.logger = logger
-
+    def detect_os(self) -> str:
+        os_name = (platform.system()).lower()
+        if "windows" in os_name :
+            return "Windows"
+        elif  "darwin" in os_name:
+            return "macOS"
+        elif  "linux" in os_name:
+            return "Linux"
+        else:
+            raise Exception(f"Unsupported operating system: {os_name}")
     def run_command(self, command: List[str], error_message: str = "Command failed"):
         """Run shell command with error handling"""
+        self.os_type = self.detect_os()
+        if self.os_type == "Windows":
+            full_command = ["powershell"] + command
+        else:
+            full_command = command
         try:
             result = subprocess.run(
-                command, 
+                full_command, 
                 check=True, 
                 capture_output=True, 
                 text=True
